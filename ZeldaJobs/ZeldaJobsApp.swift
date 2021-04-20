1de8ff8cf5f06f7ddeb7a15752dcd092970f45d4
HossamSherraa <54014933+HossamSherraa@users.noreply.github.com>
2021-04-20 21:58:07 +0200

    Initial Commit

---
+++ ZeldaJobs/ZeldaJobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeldaJobsApp.swift
+//  ZeldaJobs
+//
+//  Created by Hossam on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ZeldaJobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
