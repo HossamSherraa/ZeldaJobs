941a5ec9dddec5566abe22d5ecb70df3b6c613b7
HossamSherraa <54014933+HossamSherraa@users.noreply.github.com>
2021-05-01 17:22:59 +0200

    refactoring layout

--- ZeldaJobs/JobsCollectionViewLayout.swift
+++ ZeldaJobs/JobsCollectionViewLayout.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 class JobsCollectionViewlayout : UICollectionViewLayout {
-    var attributes : [UICollectionViewLayoutAttributes] = []
-    var contentSize : CGSize = .zero
-    let itemWidth : CGFloat = 231
-    let itemHeight : CGFloat = 309
-    let spacing: CGFloat = -10
-    let leadingSafeArea : CGFloat = 231
-   var isFirstLoad = true
+    private var attributes : [UICollectionViewLayoutAttributes] = []
+    private var contentSize : CGSize = .zero
+    private let itemWidth : CGFloat = 231
+    private let itemHeight : CGFloat = 309
+    private let spacing: CGFloat = -10
+    private let leadingSafeArea : CGFloat = 231
+    private var isFirstLoad = true
    
     override var collectionViewContentSize: CGSize {
         return .init(
@@ -21,36 +21,42 @@ class JobsCollectionViewlayout : UICollectionViewLayout {
             height: itemHeight)
     }
     
+    lazy var offestPerItem = (collectionViewContentSize.width - itemWidth*2) / CGFloat(collectionView!.numberOfItems(inSection: 0))
     override func prepare() {
         super.prepare()
         attributes.removeAll()
-        collectionView?.decelerationRate = .fast
         var xValue : CGFloat = itemWidth
-        let offestPerItem = (collectionViewContentSize.width - itemWidth*2) / CGFloat(collectionView!.numberOfItems(inSection: 0))
         for item in 0..<collectionView!.numberOfItems(inSection: 0) {
-            let indexPath = IndexPath(item: item, section: 0)
-            let attribute = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-            attribute.frame = .init(x: xValue, y: 0, width: itemWidth, height: itemHeight)
-            
-            let currentItemOffset =  CGFloat(item) * offestPerItem
-            let centerOfViewOffset = (collectionView!.bounds.width / 2) - (itemWidth / 2) + collectionView!.contentOffset.x - itemWidth
-            let currentRatio =  abs(currentItemOffset - centerOfViewOffset) / 1000
-            //When 0
-             let scaleValue = abs(1 - currentRatio)
-           
-            attribute.transform = .init(scaleX: scaleValue, y: scaleValue)
-            
+            let attribute = createAttributeForIndex(item: item)
+            let scaleValue = getScaleValue( index: item)
+            let frame = CGRect.init(x: xValue, y: 0, width: itemWidth, height: itemHeight)
+            attribute.setAttributesValues(scaleValue: scaleValue, frame: frame)
             xValue += spacing + itemWidth
-
             attributes.append(attribute)
         }
         
         if isFirstLoad {
-            collectionView?.setContentOffset(.init(x:attributes.first!.frame.origin.x / 2 , y:0), animated: false )
+            configCollectionViewForFirstLoad()
             isFirstLoad = false
         }
     }
     
+    func createAttributeForIndex(item : Int)->UICollectionViewLayoutAttributes {
+        let indexPath = IndexPath(item: item, section: 0)
+        let attribute = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+        return attribute
+    }
+    func configCollectionViewForFirstLoad(){
+        collectionView?.decelerationRate = .fast
+        collectionView?.setContentOffset(.init(x:attributes[1].center.x , y:0), animated: false )
+    }
+    
+    func getScaleValue( index : Int)->CGFloat{
+        let currentItemOffset =  CGFloat(index) * offestPerItem
+        let centerOfViewOffset = (collectionView!.bounds.width / 2) - (itemWidth / 2) + collectionView!.contentOffset.x - itemWidth
+        let currentRatio =  abs(currentItemOffset - centerOfViewOffset) / 1000
+         return abs(1 - currentRatio)
+    }
     
     override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
         true
@@ -62,12 +68,16 @@ class JobsCollectionViewlayout : UICollectionViewLayout {
         attributes[indexPath.row]
     }
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-         
         let targetItem =   attributes.sorted(by: {abs($0.center.x - proposedContentOffset.x) < abs($1.center.x - proposedContentOffset.x)}).first!
-         
-        
         return targetItem.center
     }
     
    
+}
+
+extension UICollectionViewLayoutAttributes {
+    func setAttributesValues(scaleValue : CGFloat , frame : CGRect){
+        self.frame = frame
+        self.transform = .init(scaleX: scaleValue, y: scaleValue)
+    }
 }
